bfa89653b4a9f1f085b0b77998eaa18d473457f2
Eduard-arv <eduard62691@gmail.com>
2024-12-11 21:21:56 -0500

    ajustes del Js y del CSS

--- JS/app.js
+++ JS/app.js
@@ -1,28 +1,35 @@
 const montoInput = document.getElementById("monto")
-
+const transacList = document.getElementById("lista-transaccion")
 const form = document.getElementById("transactionForm")
-
-// const montoValue = montoInput.value
-
-function obtenerMonto(monto){
+// Los arreglos (guardar informacion)
+const transacciones = []
+function obtenerMonto(monto) {
     // Me indique si hay error si el monto es valido o no
     // El monto no puede ser texto
     // El monto no puede ser negativo
     // Alerta que me muestre el monto que tengo
-    if (isNaN(monto) || monto<=0){
+    if (isNaN(monto) || monto <= 0) {
         alert("Este no es un número válido")
     }
     alert("El monto ingresado el válido")
     return alert(monto)
 }
-
-form.addEventListener("submit", (e)=> {
+form.addEventListener("submit", (e) => {
     e.preventDefault()
-
     const montoValue = montoInput.value
-    
-    obtenerMonto(montoValue)
-    const para = document.createElement("p");
-para.innerText = "This is a paragraph.";
+    transacciones.push(montoValue)
+    console.log(transacciones)
+    // Renderiza un array
+    transacList.innerHTML = "";
+    transacciones.forEach(elem => {
+        const listItem = document.createElement("div")
+        listItem.innerHTML = `
+        <div>
+            <span class="card-1">La transaccion es ${elem} </span>
+        </div>
+            `;
+        transacList.appendChild(listItem)
+    })
 })
+
 
